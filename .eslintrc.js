858f213daa457eac2c7c381fdd1d0581f2dbe93d
Drew <someone@somewhere.com>
2020-10-12 14:11:43 -0400

    fixed eslint with test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "project": "tsconfig.json",
+        "project": ["tsconfig.json","tsconfig.test.json"],
         "sourceType": "module"
     },
     "plugins": [
